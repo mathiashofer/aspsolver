1353e0b5f06b65483f4d1692fe47f95dfb946ca7
Mathias Hofer <mathias.hofer@gmx.net>
2017-01-05 19:14:22 +0100

    bugfix

--- src/at/mhofer/aspsolver/solver/FirstUIPConflictAnalysis.java
+++ src/at/mhofer/aspsolver/solver/FirstUIPConflictAnalysis.java
@@ -33,11 +33,11 @@ public class FirstUIPConflictAnalysis implements ConflictAnalysis {
 			}
 		}
 
+		// is used for a faster check if there are still two distinct
+		// literals available with dl(A,l1) == dl(A,l2) == maxDL
 		List<Literal> literals = conflictingNogood.getLiterals();
 		List<Literal> learned = conflictingNogood.getLiterals();
 		while (!literals.isEmpty() && literals.size() >= 2) {
-			// check if there are two distinct literals with dl(A,l1) ==
-			// dl(A,l2) == maxDL
 			Literal l1 = literals.get(0);
 			Literal l2 = literals.get(1);
 			if (decisionLevels.get(assignment, l1) != maxDL) {
@@ -51,18 +51,24 @@ public class FirstUIPConflictAnalysis implements ConflictAnalysis {
 
 			if (l1 != null && l2 != null) {
 				// here holds dl(A,l1) == dl(A,l2) == maxDL
-				
+
 				for (Literal l : literals) {
 					Nogood implicant = implicants.get(assignment, l);
 					if (decisionLevels.get(assignment, l) == maxDL && implicant != null) {
 						learned.remove(l);
 						learned.addAll(implicant.getLiterals());
 						learned.remove(l.negation());
+
+						// contains equal or less literals than "learned",
+						// since all with dl != maxDL get removed
+						literals.remove(l);
+						literals.addAll(implicant.getLiterals());
+						literals.remove(l.negation());
 					}
 				}
 			}
 		}
-		
+
 		int learnedMaxDL = 0;
 		for (Literal l : learned) {
 			int dl = decisionLevels.get(assignment, l);
@@ -70,7 +76,7 @@ public class FirstUIPConflictAnalysis implements ConflictAnalysis {
 				learnedMaxDL = dl;
 			}
 			if (dl > newDL && dl < learnedMaxDL) {
-				//find the second highest dl
+				// find the second highest dl
 				newDL = dl;
 			}
 		}
